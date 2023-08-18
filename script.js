410b6e253d2a45fd07de20e095b5720215c4c870
ericmota7 <ericmota010@gmail.com>
2023-08-18 16:14:36 -0300

    Mudei o css e o js

---
+++ script.js
@@ -0,0 +1,24 @@
+function escrevendoletra(){
+
+    function ativaLetra(elemento){
+        const arrTexto = elemento.innerHTML.split('');
+        elemento.innerHTML = '';
+        arrTexto.forEach((Letra, i) =>{
+            setTimeout(()=>{
+                elemento.innerHTML += Letra;
+            }, 75 * i);
+        });
+    
+    }
+    
+    const titulo = document.querySelector('.digitando');
+    ativaLetra(titulo);
+    }
+    
+    
+    const ativaMenu = document.querySelector('.fa-bars');
+    
+    ativaMenu.addEventListener('click', ()=>{
+        ativaMenu.classList.toggle('fa-x')
+    })
+    
