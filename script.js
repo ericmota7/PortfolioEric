00689b75323c210008623fcd97b87e3b4c4a1ce1
ericmota7 <ericmota010@gmail.com>
2023-09-21 12:35:55 -0300

    parte 2 portfolio

--- script.js
+++ script.js
@@ -17,8 +17,10 @@ function escrevendoletra(){
     
     
     const ativaMenu = document.querySelector('.fa-bars');
+    const navMenu = document.querySelector('header .navegacao-primaria ')
     
     ativaMenu.addEventListener('click', ()=>{
         ativaMenu.classList.toggle('fa-x')
+        navMenu.classList.toggle('ativado')
     })
     
